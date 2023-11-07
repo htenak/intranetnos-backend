69318c5d09275b6ed561fa62bde3771297050a31
kaneth <kanethplayer@gmail.com>
2023-11-06 23:43:04 -0500

    creo nueva ruta para profesores

--- src/professor/professor.controller.ts
+++ src/professor/professor.controller.ts
@@ -1,19 +1,45 @@
-import { Controller, Get, HttpStatus, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Request,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ClassService } from 'src/class/class.service';
+
 import { AuthAndRoles } from 'src/common/decorators';
 import { ROLE } from 'src/config/constants';
+import { ScheduleService } from 'src/schedule/schedule.service';
 
 @ApiTags('Professor routes')
 @Controller('professor')
 export class ProfessorController {
-  constructor(private readonly classService: ClassService) {}
+  constructor(
+    private readonly classService: ClassService,
+    private readonly scheduleService: ScheduleService,
+  ) {}
 
   // obtiene clases del profesor
   @AuthAndRoles(ROLE.professor)
   @Get('classes')
   async getClassesProfessor(@Request() req: any) {
     const data = await this.classService.getClassesProfessor(req.user.sub);
+    return { statusCode: HttpStatus.OK, data };
+  }
+
+  @AuthAndRoles(ROLE.professor)
+  @Get('other-classes')
+  async getOtherClassesProfessor(@Request() req: any) {
+    const data = await this.classService.getOtherClassesProfessores();
+    return { statusCode: HttpStatus.OK, data };
+  }
+
+  @AuthAndRoles(ROLE.professor)
+  @Get('schedules-by-class/:id')
+  async getSchedulesByClassId(@Param('id', ParseIntPipe) classId: number) {
+    const data = await this.scheduleService.getSchedulesByClassId(classId);
     return { statusCode: HttpStatus.OK, data };
   }
 }
--- src/professor/professor.module.ts
+++ src/professor/professor.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProfessorService } from './professor.service';
 import { ProfessorController } from './professor.controller';
 import { ClassModule } from 'src/class/class.module';
+import { ScheduleModule } from 'src/schedule/schedule.module';
 
 @Module({
-  imports: [ClassModule],
+  imports: [ClassModule, ScheduleModule],
   providers: [ProfessorService],
   controllers: [ProfessorController],
 })
